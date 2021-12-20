78e98b512a59f816fada7f892c306e0971fad3d0
시원해 <705lo@naver.com>
2021-12-20 13:40:09 +0000

    BOJ 2444

---
+++ 0x02/2444.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N - i; j++) {
+			cout << ' ';
+		}
+		for (int k = 1; k <= 2 * i - 1; k++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	
+	for (int i = N - 1; i >= 1; i--) {
+		for (int j = 1; j <= N - i; j++) {
+			cout << ' ';
+		}
+		for (int k = 1; k <= 2 * i - 1; k++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
