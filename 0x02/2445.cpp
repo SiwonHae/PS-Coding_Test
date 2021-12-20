803ef109a6d07d450e7283c879bd66950ba0e15f
시원해 <705lo@naver.com>
2021-12-20 13:40:15 +0000

    BOJ 2445

---
+++ 0x02/2445.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int N;
+	cin >> N;
+	
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= i; j++) {
+			cout << '*';
+		}
+		for (int k = 1; k <= 2 * (N - i); k++) {
+			cout << ' ';
+		}
+		for (int y = 1; y <= i; y++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	
+	for (int i = N - 1; i >= 1; i--) {
+		for (int j = 1; j <= i; j++) {
+			cout << '*';
+		}
+		for (int k = 1; k <= 2 * (N - i); k++) {
+			cout << ' ';
+		}
+		for (int y = 1; y <=i; y++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	
+	return 0;
+}
