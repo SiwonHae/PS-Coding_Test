da6d6bf1c869dae2955f3659295a6c2f681cfdf4
시원해 <705lo@naver.com>
2021-12-20 07:35:42 +0000

    BOJ 15552

---
+++ 0x02/15552.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int T;
+	cin >> T;
+	int A, B;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		cout << A + B << '\n';
+	}
+	
+	return 0;
+}
