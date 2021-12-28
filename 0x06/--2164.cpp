2a1bf390bb1a7ff263cd74904d1eda5b4868286f
시원해 <705lo@naver.com>
2021-12-28 13:31:41 +0000

    BOJ 2164

---
+++ 0x06/--2164.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int N;
+	cin >> N;
+	
+	queue<int> Q;
+	for (int i = 1; i <= N; i++) {
+		Q.push(i);
+	}
+	
+	while (Q.size() != 1) {
+		Q.pop();
+		int temp = Q.front();
+		Q.push(temp);
+		Q.pop();
+	}
+	
+	cout << Q.front();
+	
+	return 0;
+}
