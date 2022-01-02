d1568cbd8c0872bca0258151d81e44a15a313f98
시원해 <705lo@naver.com>
2022-01-02 07:44:15 +0000

    Update BOJ 1074

--- 0x0B/--1074.cpp
+++ 0x0B/--1074.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// https://cocoon1787.tistory.com/376 참고
 int func(int n, int r, int c) {
 	if (n == 0) {
 		return 0;
@@ -9,12 +10,14 @@ int func(int n, int r, int c) {
 	int half = 1 << (n - 1); // 한 변 길이의 절반
 	
 	if (r < half && c < half) // 1사
-		return func(n-1, r, c);
-	if (r < half && c >= half) // 2사
-		return half * half + func(n - 1, r, c - half);
-	if (r >= half && c < half) // 3사
-		return 2 * half * half + func(n - 1, r - half, c);
-	return 3 * half * half + func(n - 1, r - half, c - half); // 4사
+		return half * half * 0 + func(n - 1, r, c);
+	else if (r < half && c >= half) // 2사
+		return half * half * 1 + func(n - 1, r, c - half);
+	else if (r >= half && c < half) // 3사
+		return half * half * 2 + func(n - 1, r - half, c);
+	else // 4사
+		return half * half * 3 + func(n - 1, r - half, c - half); 
+	
 }
 
 int main(void) {
