648ce4f04cd3c00a869861b941f5a1ccc93e8059
시원해 <705lo@naver.com>
2022-01-02 07:37:04 +0000

    BOJ 1074

---
+++ 0x0B/--1074.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int func(int n, int r, int c) {
+	if (n == 0) {
+		return 0;
+	}
+	
+	int half = 1 << (n - 1); // 한 변 길이의 절반
+	
+	if (r < half && c < half) // 1사
+		return func(n-1, r, c);
+	if (r < half && c >= half) // 2사
+		return half * half + func(n - 1, r, c - half);
+	if (r >= half && c < half) // 3사
+		return 2 * half * half + func(n - 1, r - half, c);
+	return 3 * half * half + func(n - 1, r - half, c - half); // 4사
+}
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, r, c;
+	cin >> n >> r >> c;
+	cout << func(n, r, c);
+	
+	return 0;
+}
