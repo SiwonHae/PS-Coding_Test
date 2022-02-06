557a6dc908634e05acbcbdfe3fc136bc5fd3ed54
시원해 <705lo@naver.com>
2022-02-06 05:47:26 +0000

    BOJ 5397

---
+++ 0x04/5397.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n;
+	cin >> n;
+	
+	while (n--) {
+		string s;
+		cin >> s;
+		
+		list<char> L = {};
+		auto cur = L.begin();
+		
+		for (auto c : s) {
+			if (c == '<') {
+				if (cur != L.begin()) {
+					cur--;
+				}
+			} else if (c == '>') {
+				if (cur != L.end()) {
+					cur++;
+				}
+			} else if (c == '-') {
+				if (cur != L.begin()) {
+					cur--;
+					cur = L.erase(cur);
+				}
+			} else {
+				L.insert(cur, c);
+			}
+		}
+		
+		for (auto c : L) {
+			cout << c;
+		}
+		cout << '\n';
+	}
+
+	return 0;
+}
